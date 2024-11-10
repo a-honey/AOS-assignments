9658582a7044d1fe4a827b935f2316f3f54025a1
a-honey <jungahyeon0512@khu.ac.kr>
2024-11-10 21:42:33 +0900

    기본 컴포넌트 1차 구성

--- ahyeon/week4/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ ahyeon/week4/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +34,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FullScreenContent()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FullScreenContent() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)){
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.Yellow)){
+                Text(text="이미지", modifier = Modifier.align(Alignment.Center))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(modifier = Modifier.fillMaxWidth()){
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    label={Text("이름을 입력해주세요")},
+                    modifier = Modifier.fillMaxWidth())
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    label={Text("나이를 입력해주세요")},
+                    modifier = Modifier.fillMaxWidth())
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    label={Text("학교를 입력해주세요")},
+                    modifier = Modifier.fillMaxWidth())
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    label={Text("별명을 입력해주세요")},
+                    modifier = Modifier.fillMaxWidth())
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    label={Text("MBTI를 입력해주세요")},
+                    modifier = Modifier.fillMaxWidth())
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        FullScreenContent()
     }
 }
